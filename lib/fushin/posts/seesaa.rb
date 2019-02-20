9cba373d7b2f2d78c3c2db31adf95b87d76b75ca
Manabu Niseki <manabu.niseki@gmail.com>
2019-02-20 10:44:03 +0800

    feat: improve seesaa.net support
    Add attachment handing support

--- lib/fushin/posts/seesaa.rb
+++ lib/fushin/posts/seesaa.rb
@@ -10,6 +10,16 @@ module Fushin
       def main_cleanup_selectors
         %w(script iframe .listCategoryArticle font)
       end
+
+      def attachements
+        @attachements ||= doc.css("div.text > a").map do |a|
+          url = a.get("href")
+          next unless url
+          next unless url.include? "seesaa.net/image/"
+
+          Models::Attachment.new url
+        end.compact
+      end
     end
   end
 end
